792b7d60da55294aea94752f69f9e723d785a6bd
skytomo <tomo1kawa.fullerene@gmail.com>
2020-03-20 21:09:26 +0900

    Add other unit test

--- OtamajakushiUnitTestProject/UnitTest1.cs
+++ OtamajakushiUnitTestProject/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Otamajakushi;
 
@@ -8,12 +10,46 @@ namespace OtamajakushiUnitTestProject
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        /// 読み込みテスト
+        /// </summary>
         [TestMethod]
         public void TestMethod1()
         {
             var dictionary = OneToManyJsonSerializer.Deserialize(File.ReadAllText(@".\..\..\test.json"));
-            File.WriteAllText(@".\..\..\output.json", OneToManyJsonSerializer.Serialize(dictionary));
             Assert.AreEqual(1, dictionary.Words.Count);
+            Assert.AreEqual("lipalain", dictionary.Words[0].Entry.Form);
+            Assert.AreEqual("【名詞】lipalainerfemo リパライン語化する\nlipalain chafi'ofes　リパラオネ共和国",
+                dictionary.Words[0].Contents.Find(x => x.Title == "語法").Text);
+        }
+
+        /// <summary>
+        /// 書き込みテスト
+        /// </summary>
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var dictionary = OneToManyJsonSerializer.Deserialize(File.ReadAllText(@".\..\..\test.json"));
+            var options = new System.Text.Json.JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+                WriteIndented = true,
+            };
+            var json = OneToManyJsonSerializer.Serialize(dictionary, options);
+            File.WriteAllText(@".\..\..\output.json", json);
+        }
+
+        /// <summary>
+        /// 書き込んだファイルの読み込みテスト
+        /// </summary>
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var dictionary = OneToManyJsonSerializer.Deserialize(File.ReadAllText(@".\..\..\output.json"));
+            Assert.AreEqual(1, dictionary.Words.Count);
+            Assert.AreEqual("lipalain", dictionary.Words[0].Entry.Form);
+            Assert.AreEqual("【名詞】lipalainerfemo リパライン語化する\nlipalain chafi'ofes　リパラオネ共和国",
+                dictionary.Words[0].Contents.Find(x => x.Title == "語法").Text);
         }
     }
 }
