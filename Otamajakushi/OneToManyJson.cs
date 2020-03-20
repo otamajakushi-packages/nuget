36e4d8c7ce0f663388b65bb1b56a948411fe3de3
skytomo <tomo1kawa.fullerene@gmail.com>
2020-03-20 16:37:46 +0900

    Add Serializer

--- Otamajakushi/OneToManyJson.cs
+++ Otamajakushi/OneToManyJson.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text.Json;
 
 namespace Otamajakushi
 {
---
+++ Otamajakushi/OneToManyJsonSerializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace Otamajakushi
+{
+    public static class OneToManyJsonSerializer
+    {
+        public static OneToManyJson Deserialize(string json, JsonSerializerOptions options = null)
+        {
+            return JsonSerializer.Deserialize<OneToManyJson>(json, options);
+        }
+
+        public static string Serialize(OneToManyJson value, JsonSerializerOptions options = null)
+        {
+            return JsonSerializer.Serialize(value, options);
+        }
+    }
+}
