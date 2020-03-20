65e4ff2cef832e13855adf4e8d39306f01cde0ba
skytomo <tomo1kawa.fullerene@gmail.com>
2020-03-20 19:59:21 +0900

    Add unit test

---
+++ OtamajakushiUnitTestProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Otamajakushi;
+
+namespace OtamajakushiUnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var dictionary = OneToManyJsonSerializer.Deserialize(File.ReadAllText(@".\..\..\test.json"));
+            File.WriteAllText(@".\..\..\output.json", OneToManyJsonSerializer.Serialize(dictionary));
+            Assert.AreEqual(1, dictionary.Words.Count);
+        }
+    }
+}
